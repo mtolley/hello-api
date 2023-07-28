a3de2a063cbebf15ed7e7666efce16c708b1e345
Scott Tolley <michaeltolley21@hotmail.com>
2023-07-28 10:53:34 +0200

    Adding AppEngine deployment

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/mtolley/hello-api/handlers"
 	"github.com/mtolley/hello-api/handlers/rest"
 )
 
@@ -11,6 +12,7 @@ func main() {
 	addr := ":8080"
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", rest.TranslateHandler)
+	mux.HandleFunc("/health", handlers.HealthCheck)
 	log.Printf("Listening on %s", addr)
 	log.Fatal(http.ListenAndServe(addr, mux))
 }
